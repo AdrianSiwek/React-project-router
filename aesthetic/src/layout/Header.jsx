ad717b3d16b96ff61a1ec0d5c5046f727be13e28
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-04-02 23:26:06 +0200

    update

--- aesthetic/src/layout/Header.jsx
+++ aesthetic/src/layout/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import styles from '../styles/Header.module.css'
+import img1 from '../images/image1.png';
+import img2 from '../images/image2.png';
+import img3 from '../images/image3.png';
 
 const Header = () => {
     return ( 
-        <div>Header</div>
+        <div className={styles.header}>
+            <img src={img1} alt="logopedia" className={styles.headerImg} />
+        </div>
      );
 }
  
--- aesthetic/src/layout/Navigation.jsx
+++ aesthetic/src/layout/Navigation.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '../styles/Navigation.module.css';
+
+
+const list = [
+    { name: 'Strona Główna', path: '/strona', exact: true},
+    { name: 'Produkty', path: '/products' },
+    { name: 'Kontakt', path: '/contact' },
+    {name: 'Panel Admina', path: '/panel admina'},
+]
+
 
 const Navigation = () => {
+
+    const menu = list.map((item) => (
+        <li key={item.name}>
+            <NavLink to={item.path} exact={item.exact? item.exact:false}>{item.name}
+            </NavLink>
+        </li>
+    ))
+
     return ( 
-        <div>Navi</div>
+        <nav className={styles.navi}>
+            <ul className={styles.menu}>
+               {menu} 
+            </ul>
+        </nav>
      );
 }
  
